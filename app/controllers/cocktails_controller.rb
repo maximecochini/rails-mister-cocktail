88ca0a280cea917cb970dcc14af857c3722221d4
Maxime Cochini <maxime.cochini@gmail.com>
2020-05-28 14:17:56 +0200

    Cocktails Controller

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,24 +1,39 @@
 class CocktailsController < ApplicationController
   before_action :set_cocktail, only: [:show, :edit, :update, :destroy]
+  
   def index
+    @cocktails = Cocktail.all
   end
 
   def show
   end
 
   def new
+    @cocktail = Cocktail.new
   end
 
   def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render "new"
+    end
   end
 
   def edit
   end
 
   def update
+    @cocktail.update(cocktail_params)
+
+    redirect_to cocktail_path
   end
 
   def destroy
+    @cocktail.destroy
+
+    redirect_to cocktails_path
   end
 
   private
@@ -27,7 +42,7 @@ class CocktailsController < ApplicationController
     params.require(:cocktail).permit(:name)
   end
 
-  def set_restaurant
-    @cocktail = Restaurant.find(params[:id])
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:id])
   end
 end
