94ba23117cdea45ec2088cafb2bc1233a65ba5de
Maxime Cochini <maxime.cochini@gmail.com>
2020-05-28 12:03:25 +0200

    Make Ingredient, Dose and Cocktail models validations and junctions.

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,2 +1,15 @@
 class Ingredient < ApplicationRecord
+  before_destroy :ensure_no_children
+
+  has_many :doses
+
+  validates :name, uniqueness: true, presence: true
+
+  private
+
+  def ensure_no_children
+    unless self.doses.empty?
+      raise ActiveRecord::InvalidForeignKey
+    end
+  end
 end
