d52b79252003575959c34c83730c4665dfe5e9f1
Maxime Cochini <maxime.cochini@gmail.com>
2020-05-28 12:54:55 +0200

    Seeded Ingredients

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,4 +1,5 @@
 class CocktailsController < ApplicationController
+  before_action :set_cocktail, only: [:show, :edit, :update, :destroy]
   def index
   end
 
@@ -18,5 +19,15 @@ class CocktailsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
+
+  def set_restaurant
+    @cocktail = Restaurant.find(params[:id])
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,53 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+cocktails_number = 10
+ingredients_max_rand_number = 4
+
+cocktaildb_url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+ingredients = JSON.parse(open(cocktaildb_url).read)['drinks']
+ingredients = ingredients.map { |x| x.values }.flatten
+
+def main_separator
+  puts "\n\n" + "*" * 50 + "\n\n"
+end
+
+def alt_separator
+  puts "-" * 50 + "\n\n"
+end
+
+
+puts "Seeding started."
+
+main_separator
+
+puts "Adding Ingredients..."
+alt_separator
+ingredients.each do |ing|
+  Ingredient.create(name: ing)
+  puts "-- Added #{ing}."
+end
+alt_separator
+puts "Done"
+
+main_separator
+
+puts "Cleaning database..."
+alt_separator
+puts "-- Cleaning Cocktails..."
+# Cocktail.destroy_all
+# alt_separator
+# puts "-- Cleaning Ingredients..."
+# Ingredient.destroy_all
+puts "Database is now clean."
+
+main_separator
+
+puts "Creating cocktails..."
+# cocktails_number.times do
+# end
+
+puts "Finished!"
