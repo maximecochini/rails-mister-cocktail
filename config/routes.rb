5a98253fab1628d59e5d80754690a509e124eaf0
Maxime Cochini <maxime.cochini@gmail.com>
2020-05-29 11:09:02 +0200

    Config Heroku & Cloudinary

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :cocktails
+  resources :cocktails do
+    resources :doses, only: [ :index, :new, :create ]
+  end
+  resources :doses, only: [ :show, :edit, :update, :destroy ]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
